cf8a8889c0bc2960a700557684f9049da45814ce
mindsetcloud <70407614+mindsetcloud@users.noreply.github.com>
2022-10-26 20:25:37 -0300

    Update install.sh

--- docker/stream-sets/install.sh
+++ docker/stream-sets/install.sh
@@ -6,3 +6,7 @@ apt install openjdk-8-jdk
 #https://accounts.streamsets.com/archives
 wget https://archives.streamsets.com/datacollector/3.22.3/tarball/activation/streamsets-datacollector-all-3.22.3.tgz
 mv streamsets-datacollector /opt/stramsets
+
+cd /opt/streamsets
+bin/streamsets dc
+
