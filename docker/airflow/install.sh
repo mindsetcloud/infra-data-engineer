77245cd4cbe2bcb9313476501817f09e259f01e0
mindsetcloud <70407614+mindsetcloud@users.noreply.github.com>
2023-03-22 09:47:40 -0300

    Update install.sh

--- docker/airflow/install.sh
+++ docker/airflow/install.sh
@@ -10,8 +10,18 @@ PYTHON_VERSION="$(python3 --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 # For example: 3.7
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 # For example: https://raw.githubusercontent.com/apache/airflow/constraints-2.5.1/constraints-3.7.txt
-pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+pip install "apache-airflow[postgresql]==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
 
+#install library postgres to connect on sqlalchemy
+pip install psycopg2-binary
+
+#edit file airflow.cfg
+sql_alchemy_conn = postgresql://airflow:airflow@localhost/airflow
+
+#init database
+airflow db init
+
+#
 # The Standalone command will initialise the database, make a user,
 # and start all components for you.
 airflow standalone
