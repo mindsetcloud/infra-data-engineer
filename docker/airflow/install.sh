da07d883204071974b1f8b2b08d6dab54e149704
mindsetcloud <70407614+mindsetcloud@users.noreply.github.com>
2023-03-25 18:50:26 -0300

    Update install.sh

--- docker/airflow/install.sh
+++ docker/airflow/install.sh
@@ -10,11 +10,15 @@ PYTHON_VERSION="$(python3 --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 # For example: 3.7
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 # For example: https://raw.githubusercontent.com/apache/airflow/constraints-2.5.1/constraints-3.7.txt
-pip install "apache-airflow[postgresql]==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+
+
 
 #install library postgres to connect on sqlalchemy
-pip install psycopg2
 pip install psycopg2-binary
+pip install apache-airflow-providers-common-sql
+
+pip install apache-airflow-providers-postgres
 
 #edit file airflow.cfg
 sql_alchemy_conn = postgresql://airflow:airflow@localhost/airflow
